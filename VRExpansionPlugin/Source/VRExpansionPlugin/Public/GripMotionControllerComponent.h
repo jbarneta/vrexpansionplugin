855bc137c62a364ee9269965cda1cb7a5134882b
morden <morden@noxhub.net>
2018-07-15 20:52:31 -0400

    fixing late update offset with controller profiles

--- VRExpansionPlugin/Source/VRExpansionPlugin/Public/GripMotionControllerComponent.h
+++ VRExpansionPlugin/Source/VRExpansionPlugin/Public/GripMotionControllerComponent.h
@@ -146,6 +146,7 @@ public:
 
 protected:
 	//~ Begin UActorComponent Interface.
+	virtual void CreateRenderState_Concurrent() override;
 	virtual void SendRenderTransform_Concurrent() override;
 	//~ End UActorComponent Interface.
 
